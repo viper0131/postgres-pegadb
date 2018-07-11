44860c0bb486608bb06e7537d6d489d33305d1e2
Machiel <machiel.van.ampting@trivento.nl>
2018-07-11 22:44:54 +0200

    init-db

---
+++ init/02_init.sql
@@ -0,0 +1,4 @@
+alter role pegabase set search_path to pegadata,pegarules,sqlj;
+/c pega
+alter role pegabase set pljava.libjvm_location to '/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server/libjvm.so'
+
